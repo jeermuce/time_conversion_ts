17723aa79d179ac97116f4250b6556e7f0a405b3
jeermuce <ernesto_mc.dev@proton.me>
2023-12-21 13:04:59 -0600

    Complete exercise

--- time_conversion_ts.js
+++ time_conversion_ts.js
@@ -1,1 +1,92 @@
-console.log("Hello World!");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+var inputString = "";
+var inputLines = [];
+var currentLine = 0;
+process.stdin.on("data", function (inputStdin) {
+    inputString += inputStdin;
+});
+process.stdin.on("end", function () {
+    inputLines = inputString.split("\n");
+    inputString = "";
+    main();
+});
+function readLine() {
+    return inputLines[currentLine++];
+}
+function timeConversion(s) {
+    // Write your code here
+    var timeAMPM = parseAMPM(s);
+    var timeMilitary = convertToMilitaryTime(timeAMPM);
+    return formatMilitaryTime(timeMilitary);
+}
+function convertToMilitaryTime(time) {
+    var hours = time.hours, minutes = time.minutes, seconds = time.seconds, period = time.period;
+    if (period === "AM") {
+        return { hours: hours, minutes: minutes, seconds: seconds };
+    }
+    else if (period === "PM") {
+        return { hours: hours + 12 < 24 ? hours + 12 : 0, minutes: minutes, seconds: seconds };
+    }
+    else {
+        throwerror("Period not not recognized", period);
+    }
+}
+function formatMilitaryTime(time) {
+    var hours = time.hours, minutes = time.minutes, seconds = time.seconds;
+    var hours_str = hours.toString();
+    var minutes_str = minutes.toString();
+    var seconds_str = seconds.toString();
+    if (hours < 10)
+        hours_str = "0".concat(hours);
+    if (minutes < 10)
+        minutes_str = "0".concat(minutes);
+    if (seconds < 10)
+        seconds_str = "0".concat(seconds);
+    return "".concat(hours_str, ":").concat(minutes_str, ":").concat(seconds_str);
+}
+function parseAMPM(string) {
+    var newArray = string.split(":");
+    var hours = parseAMPMHours(parseInt(newArray[0]));
+    var minutes = parseMinutesOrSeconds(parseInt(newArray[1]));
+    var seconds = parseMinutesOrSeconds(parseInt(newArray[2]));
+    var period = newArray[2].slice(2).toUpperCase();
+    return { hours: hours, minutes: minutes, seconds: seconds, period: period };
+}
+function throwerror(msg, problem) {
+    throw new Error("Error: ".concat(msg, ", problem: ").concat(problem));
+}
+function parseAMPMHours(number) {
+    //throw error if number is not between 0 and 12
+    if (number < 0 || number > 12) {
+        throwerror("Hours out of range", number);
+    }
+    if (number === 12)
+        return 0;
+    else
+        return number;
+}
+function parseMinutesOrSeconds(number) {
+    if (number < 0 || number > 60)
+        throwerror("Minutes or seconds out of range", number);
+    else
+        return number;
+}
+function main() {
+    var s = "07:05:45PM";
+    var result = timeConversion(s);
+    console.log("input: ".concat(s, ", output: ").concat(result));
+    s = "12:01:02AM";
+    result = timeConversion(s);
+    console.log("input: ".concat(s, ", output: ").concat(result));
+    s = "12:01:02PM";
+    result = timeConversion(s);
+    console.log("input: ".concat(s, ", output: ").concat(result));
+    s = "13:01:02PM";
+    result = timeConversion(s);
+    console.log("input: ".concat(s, ", output: ").concat(result));
+    return;
+}
+main();
--- time_conversion_ts.ts
+++ time_conversion_ts.ts
@@ -1,1 +1,127 @@
-console.log("Hello World!");
+"use strict";
+
+import { error } from "console";
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString: string = "";
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on("data", function (inputStdin: string): void {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function (): void {
+  inputLines = inputString.split("\n");
+  inputString = "";
+
+  main();
+});
+
+function readLine(): string {
+  return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+type NumOrStr = number | string;
+type MilitaryHours = number;
+type AMPMHours = number;
+type MinutesOrSeconds = number;
+type Period = "AM" | "PM";
+interface MilitaryTime {
+  hours: MilitaryHours;
+  minutes: MinutesOrSeconds;
+  seconds: MinutesOrSeconds;
+}
+// takes minutes and seconds from military time
+interface AMPMTime {
+  hours: AMPMHours;
+  minutes: MinutesOrSeconds;
+  seconds: MinutesOrSeconds;
+  period: Period;
+}
+
+function timeConversion(s: string): string {
+  // Write your code here
+  let timeAMPM: AMPMTime = parseAMPM(s);
+  let timeMilitary: MilitaryTime = convertToMilitaryTime(timeAMPM);
+  return formatMilitaryTime(timeMilitary);
+}
+function convertToMilitaryTime(time: AMPMTime): MilitaryTime {
+  const { hours, minutes, seconds, period } = time;
+
+  if (period === "AM") {
+    return { hours, minutes, seconds };
+  } else if (period === "PM") {
+    return { hours: hours + 12 < 24 ? hours + 12 : 0, minutes, seconds };
+  } else {
+    throwerror("Period not not recognized", period);
+  }
+}
+function formatMilitaryTime(time: MilitaryTime): string {
+  let { hours, minutes, seconds } = time;
+  let hours_str: string = hours.toString();
+  let minutes_str: string = minutes.toString();
+  let seconds_str: string = seconds.toString();
+  if (hours < 10) hours_str = `0${hours}`;
+  if (minutes < 10) minutes_str = `0${minutes}`;
+  if (seconds < 10) seconds_str = `0${seconds}`;
+
+  return `${hours_str}:${minutes_str}:${seconds_str}`;
+}
+
+function parseAMPM(string: string): AMPMTime {
+  const newArray = string.split(":");
+  const hours: AMPMHours = parseAMPMHours(parseInt(newArray[0]));
+  const minutes: MinutesOrSeconds = parseMinutesOrSeconds(
+    parseInt(newArray[1])
+  );
+  const seconds: MinutesOrSeconds = parseMinutesOrSeconds(
+    parseInt(newArray[2])
+  );
+  const period: Period = newArray[2].slice(2).toUpperCase() as Period;
+  return { hours, minutes, seconds, period };
+}
+function throwerror(msg: string, problem: NumOrStr): never {
+  throw new Error(`Error: ${msg}, problem: ${problem}`);
+}
+function parseAMPMHours(number: number): AMPMHours {
+  //throw error if number is not between 0 and 12
+  if (number < 0 || number > 12) {
+    throwerror("Hours out of range", number);
+  }
+  if (number === 12) return 0;
+  else return number;
+}
+
+function parseMinutesOrSeconds(number: number): MinutesOrSeconds {
+  if (number < 0 || number > 60)
+    throwerror("Minutes or seconds out of range", number);
+  else return number as MinutesOrSeconds;
+}
+
+function main() {
+  let s = "07:05:45PM";
+  let result = timeConversion(s);
+  console.log(`input: ${s}, output: ${result}`);
+
+  s = "12:01:02AM";
+  result = timeConversion(s);
+  console.log(`input: ${s}, output: ${result}`);
+
+  s = "12:01:02PM";
+  result = timeConversion(s);
+  console.log(`input: ${s}, output: ${result}`);
+  s = "13:01:02PM";
+  result = timeConversion(s);
+  console.log(`input: ${s}, output: ${result}`);
+  return;
+}
+main();
